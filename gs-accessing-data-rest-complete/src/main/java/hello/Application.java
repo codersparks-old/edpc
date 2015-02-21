cb60b7aeb4448b6ebd04a6084472a4a4d919c5ba
codersparks <codersparks@gmail.com>
2015-02-21 19:56:03 +0000

    Initial commit

---
+++ gs-accessing-data-rest-complete/src/main/java/hello/Application.java
@@ -0,0 +1,29 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+@Configuration
+@EnableJpaRepositories
+@Import(RepositoryRestMvcConfiguration.class)
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // Customize the application or call application.sources(...) to add sources
+        // Since our example is itself a @Configuration class we actually don't
+        // need to override this method.
+        return application.sources(Application.class);
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
