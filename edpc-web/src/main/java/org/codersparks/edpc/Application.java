512e1c4b0030c00f8e24dc8be7584ea75a3d860d
codersparks <codersparks@gmail.com>
2015-02-21 20:48:02 +0000

    Added base uri back again

--- edpc-web/src/main/java/org/codersparks/edpc/Application.java
+++ edpc-web/src/main/java/org/codersparks/edpc/Application.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
+@ComponentScan
 @Configuration
 @EnableJpaRepositories
-@Import(RepositoryRestMvcConfiguration.class)
 @EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 
