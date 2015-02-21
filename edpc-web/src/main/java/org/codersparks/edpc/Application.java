3a32484029ad991a974ad7aeca70f367ce4d5fae
codersparks <codersparks@gmail.com>
2015-02-21 20:56:14 +0000

    Still trying to get base uri to change

--- edpc-web/src/main/java/org/codersparks/edpc/Application.java
+++ edpc-web/src/main/java/org/codersparks/edpc/Application.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @ComponentScan
 @Configuration
+@Import(RestDataConfig.class)
 @EnableJpaRepositories
 @EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
